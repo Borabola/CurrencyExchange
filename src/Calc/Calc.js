0d992a25bd212df4ef0a59007d4edfd7698f0dc7
Olya <osokolova1@gmail.com>
2020-08-16 13:35:45 +0300

    Расчет покупки валюты

--- src/Calc/Calc.js
+++ src/Calc/Calc.js
@@ -5,8 +5,23 @@ class Calc extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
+          result: 0,
           
       }
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        return { rate:props.rate };
+    }
+
+    calcRate = (evt) => {
+        evt.preventDefault();
+        let elements = evt.target.elements;
+        console.log(elements["count-currency"].value);
+        console.log(elements["type-currency"].value);
+        let countCurrency = elements["count-currency"].value;
+        let typeCurrency = elements["type-currency"].value;
+        this.setState({result: ((countCurrency / this.state.rate[typeCurrency]).toFixed(2))})
     }
 
    
@@ -16,31 +31,27 @@ class Calc extends React.Component {
                <h3> Калькулятор обмена</h3>
                         <div className="block">
                             <div>Я хочу</div>
-                            <div><label><input type="radio" name="radio" defaultValue="0" /> купить</label></div>
-                            <div><label><input type="radio" name="radio" defaultValue="1" /> продать</label></div>
                             <div>
-                                <input type="number" defaultValue="150" />
-                                <select name="" id="">
-                                    <option defaultValue="USD">USD</option>
-                                    <option defaultValue="RUB">RUB</option>
-                                    <option defaultValue="EUR">EUR</option>
-                                    {Object.keys(this.props.rate).map((keyName, i) => 
-                                    (
-                                    <option  key={keyName} value={keyName}>{keyName}</option>
-                        
-                                    ))}
-                                </select>
+                                    <form onSubmit={this.calcRate}>
+                                        <input type="number" defaultValue="150" name= "count-currency"/>
+                                        <select name="type-currency" id="">
+                                            {Object.keys(this.props.rate).map((keyName, i) => 
+                                            (
+                                            <option  key={keyName} value={keyName}>{keyName}</option>
+                                
+                                            ))}
+                                        </select>
+                                        <input type="submit" defaultValue="calc" />
+                                    </form>
+                                </div>
+                                <div>
+                                    <h4>Результат</h4>
+                                    <ul className="calc-res">
+                                        <li>{this.state.result}</li>
+                                       
+                                    </ul>
+                                </div>
                             </div>
-                            <div>
-                                <h4>Результат</h4>
-                                <ul className="calc-res">
-                                    <li>EUR 150</li>
-                                    <li>EUR 150</li>
-                                    <li>EUR 150</li>
-                                    <li>EUR 150</li>
-                                </ul>
-                            </div>
-                        </div>
                
            </div>          )
       }
